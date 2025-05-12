50effe33ef358185faf042591894d3cd26da1b80
CaduQuaglia <carlos.quaglia@sou.inteli.edu.br>
2025-05-12 11:28:03 -0300

    feat tabela curso

--- mvc-boilerplate/scripts/init.sql
+++ mvc-boilerplate/scripts/init.sql
@@ -10,30 +10,6 @@ CREATE TABLE IF NOT EXISTS users (
   email VARCHAR(100) UNIQUE NOT NULL
 );
 
--- Inserir 20 usuários com nomes e emails aleatórios
-INSERT INTO users (name, email)
-VALUES 
-  ('Alice Smith', 'alice.smith@example.com'),
-  ('Bob Johnson', 'bob.johnson@example.com'),
-  ('Carol Williams', 'carol.williams@example.com'),
-  ('David Jones', 'david.jones@example.com'),
-  ('Emma Brown', 'emma.brown@example.com'),
-  ('Frank Davis', 'frank.davis@example.com'),
-  ('Grace Wilson', 'grace.wilson@example.com'),
-  ('Henry Moore', 'henry.moore@example.com'),
-  ('Isabella Taylor', 'isabella.taylor@example.com'),
-  ('Jack Lee', 'jack.lee@example.com'),
-  ('Kate Clark', 'kate.clark@example.com'),
-  ('Liam Martinez', 'liam.martinez@example.com'),
-  ('Mia Rodriguez', 'mia.rodriguez@example.com'),
-  ('Noah Garcia', 'noah.garcia@example.com'),
-  ('Olivia Hernandez', 'olivia.hernandez@example.com'),
-  ('Patrick Martinez', 'patrick.martinez@example.com'),
-  ('Quinn Lopez', 'quinn.lopez@example.com'),
-  ('Rose Thompson', 'rose.thompson@example.com'),
-  ('Samuel Perez', 'samuel.perez@example.com'),
-  ('Tara Scott', 'tara.scott@example.com');
-
 CREATE TABLE IF NOT EXISTS aluno (
   id SERIAL PRIMARY KEY,
   nome TEXT NOT NULL,
@@ -42,3 +18,8 @@ CREATE TABLE IF NOT EXISTS aluno (
 );
 
 CREATE INDEX IF NOT EXISTS idx_aluno_email ON aluno (email);
+
+CREATE TABLE IF NOT EXISTS curso (
+  id SERIAL PRIMARY KEY,
+  nome TEXT NOT NULL
+);
